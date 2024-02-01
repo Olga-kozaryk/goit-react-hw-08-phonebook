d32d3886d8da273cc5731d58e22761c7e126d398
Olga-kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2024-02-01 20:41:18 +0000

    Deploying to gh-pages from @ Olga-kozaryk/goit-react-hw-08-phonebook@a03520f013f591e67b16876e0dcde9213afd82fb 🚀

---
+++ static/js/434.7d654f1c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[434],{434:function(e,n,o){o.r(n),o.d(n,{Phonebook:function(){return r}});o(791);var t=o(329),r=function(){return(0,t.jsx)("div",{children:"Phonebook"})}}}]);
+//# sourceMappingURL=434.7d654f1c.chunk.js.map
