4e76c6bc279194581157099891b89bf40afc0230
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2024-02-03 22:49:13 +0200

    up

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,41 +1,65 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import List from './ContactList.styled';
-import BtnDelete from './BtnDelet.styled';
-import { deleteContact, fetchContacts } from '../../redux/thunk';
-import { getContacts, getFilter } from '../../redux/selector';
-import { useEffect } from 'react';
-import Loader from 'components/Loader/Loader';
+
+import { deleteContact} from '../../redux/thunk';
+import { getFilter, getVisibleContacts } from '../../redux/selector';
+import { useState } from 'react';
+import { Box, Button, Flex, ListItem, OrderedList, Text } from '@chakra-ui/react';
+
+const HighlightText = ({ highlightText, name }) => {
+  const indexSubstr = name.toLowerCase().indexOf(highlightText);
+
+  const highlightText1 = name.slice(0, indexSubstr)
+  const highlightText2 = name.slice(indexSubstr, indexSubstr + highlightText.length)
+  const highlightText3 = name.slice(indexSubstr + highlightText.length)
+
+  return <Text as='span'>{highlightText1}<Text as='span' fontWeight='bold'>{highlightText2}</Text>{highlightText3}</Text>
+}
 
 const ContactList = () => {
-  const {items, isLoading} = useSelector(getContacts);
-  const filterValue = useSelector(getFilter);
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const filter = useSelector(getFilter)
 
-  useEffect(() => {
-    dispatch(fetchContacts());
-  },[dispatch])
+  const visibleContacts = useSelector(getVisibleContacts)
 
-  const contactDelete = (id) => {
-    dispatch(deleteContact(id));
-  }
+  const contactDelete = async (id) => {
+    await dispatch(deleteContact(id)).unwrap();
+};
 
-  const visibleContacts = items?.filter((contact) =>
-    contact?.name?.toLowerCase().includes(filterValue.toLowerCase())
-  );
 
   return (
     <>
-    {isLoading && <Loader/>}
-    
-    <List>
-      {visibleContacts.map(({ id, name, phone }) => (
-        <li key={id}>
-          {name}: {phone}
-          <BtnDelete type="button" onClick={() => contactDelete(id)}>Delete</BtnDelete>
-        </li>
+      
+    <OrderedList>
+
+      {visibleContacts.map(({ id, name, number }) => (
+      <ListItem marginBlock={6}>
+        <Flex width="100%"
+          justifyContent='space-between'
+          alignItems='center'>
+
+          <Box>
+            <Text>
+              {!!filter ? 
+              <HighlightText highlightText={filter} name={name} />
+               : name}  {number}</Text>
+
+            </Box>
+
+          <Button 
+          isLoading={isLoading} 
+          colorScheme='red' 
+          size="sm"
+          onClick={() => {
+          setIsLoading(true);
+          contactDelete(id).finally(() => setIsLoading(false))
+          }}>Delete</Button>
+
+      </Flex>
+    </ListItem>
       ))}
-    </List>
+    </OrderedList>
     </>
   );
 }
