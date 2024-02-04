0bd9be140cbdc099c861878f2f9f30bcb4081635
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2024-02-04 10:53:45 +0200

    up

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,12 +2,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { OrderedList} from '@chakra-ui/react';
 
 import { deleteContact} from '../../redux/thunk';
-import {getVisibleContacts } from '../../redux/selector';
+import {getContacts, getFilter} from '../../redux/selector';
 import ContactListItem from './ContactListItem';
 
 const ContactList = () => {
     const dispatch = useDispatch();
-    const visibleContacts = useSelector(getVisibleContacts);
+    const items= useSelector(getContacts);
+    const filterValue = useSelector(getFilter);
+
+
+    const visibleContacts = items?.filter(item =>
+    item.name.toLowerCase().includes(filterValue.toLowerCase())
+  );
 
     const handleDeleteItem = async (id) => {
         await dispatch(deleteContact(id)).unwrap();
@@ -15,9 +21,9 @@ const ContactList = () => {
 
     return (
         <OrderedList>
-            {visibleContacts.map((item) => (
+           { visibleContacts.map((item) => 
                 <ContactListItem key={item.id} item={item} onDelete={handleDeleteItem} />
-            ))}
+           )}
         </OrderedList>
     );
 }
--- src/redux/selector.js
+++ src/redux/selector.js
@@ -3,9 +3,5 @@ export const getFilter = state => state.filters;
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
 
-export const getVisibleContacts = state => {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-  return filter ? contacts.filter(({ name }) => name.toLowerCase().includes(filter)) : contacts;
-};
+
 //OK
